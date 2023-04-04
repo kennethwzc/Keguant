dd06bec0dc69bf8f830026b407e1988adfb5980b
Kenneth Wong <wongzhicongkenneth@outlook.com>
2023-04-04 17:53:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
-function handleFormSubmission() {
-  const principalInput = document.getElementById("principal");
-  const interestInput = document.getElementById("interest");
-  const tenureInput = document.getElementById("tenure");
-  const resultElement = document.getElementById("result");
+const principalInput = document.getElementById('principal');
+const interestInput = document.getElementById('interest');
 
-  const principal = parseFloat(principalInput.value.replace(/[^0-9\.]/g, ""));
-  const interest = parseFloat(interestInput.value);
-  const tenure = parseInt(tenureInput.value);
-
-  if (isNaN(principal) || isNaN(interest) || isNaN(tenure)) {
-    resultElement.textContent = "Please fill all the fields with valid values.";
-    return;
-  }
-
-  const monthlyInterest = interest / 100 / 12;
-  const totalPayments = tenure * 12;
-  const monthlyPayment = (principal * monthlyInterest) / (1 - (1 / (1 + monthlyInterest) ** totalPayments));
-
-  resultElement.textContent = `Your monthly payment is $${addCommas(monthlyPayment.toFixed(2))}.`;
-}
-
-function addCommas(num) {
-  num = num.toString().replace(/\$|\,/g,'');
-  if(isNaN(num))
-    num = "0";
-  let sign = num.includes('-') ? '-' : '';
-  num = num.replace('-', '');
-  let cents = '';
-  if(num.indexOf('.') > 0) {
-    cents = '.' + num.substr(num.indexOf('.')+1);
-    num = num.substr(0, num.indexOf('.'));
-  }
-  let thousands = /(\d+)(\d{3})/;
-  while(thousands.test(num))
-    num = num.replace(thousands, '$1'+','+'$2');
-  return sign + '$' + num + cents;
-}
-
-
-const principalInput = document.getElementById("principal");
-
-principalInput.addEventListener("input", function(e) {
-  let value = parseFloat(this.value.replace(/[^0-9\.]/g, ""));
-  if (isNaN(value)) {
-    value = "";
-  } else {
-    value = addCommas(value.toString());
-  }
-  this.value = "$" + value;
+principalInput.addEventListener('input', function() {
+  this.value = addCommas(this.value.replace(/\D/g, ''));
+  this.value = '$' + this.value;
 });
+
+interestInput.addEventListener('input', function() {
+  this.value = this.value.replace(/\D/g, '') + '%';
+});
+
+function addCommas(nStr) {
+  nStr += '';
+  const x = nStr.split('.');
+  let x1 = x[0];
+  const x2 = x.length > 1 ? '.' + x[1] : '';
+  const rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+  return x1 + x2;
+}
+
+function calculateMonthlyPayment(principal, interestRate, tenure) {
+  const monthlyInterestRate = interestRate / 100 / 12;
+  const numberOfPayments = tenure * 12;
+  const discountFactor = ((1 + monthlyInterestRate) ** numberOfPayments - 1) /
+    (monthlyInterestRate * (1 + monthlyInterestRate) ** numberOfPayments);
+  const monthlyPayment = principal / discountFactor;
+  return monthlyPayment.toFixed(2);
+}
+
+function handleFormSubmission() {
+  const principal = Number(principalInput.value.replace(/[^0-9\.]+/g,""));
+  const interestRate = Number(interestInput.value.replace(/[^0-9\.]+/g,""));
+  const tenure = Number(document.getElementById('tenure').value);
+  const monthlyPayment = calculateMonthlyPayment(principal, interestRate, tenure);
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = `Monthly payment: $${monthlyPayment}`;
+}
