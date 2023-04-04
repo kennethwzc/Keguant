32b2dde3aedf0e77aaecd19ac6079e7bfceccb2a
Kenneth Wong <wongzhicongkenneth@outlook.com>
2023-04-04 17:40:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,26 @@
-/**
- * Calculates the monthly payment and total interest payable for a loan.
- * @param {number} principal - The principal amount of the loan.
- * @param {number} interestRate - The annual interest rate of the loan as a percentage.
- * @param {number} loanTenure - The tenure of the loan in years.
- * @returns {Object} - An object containing the monthly payment and total interest payable.
- */
-function calculateLoanDetails(principal, interestRate, loanTenure) {
-  const monthlyInterestRate = interestRate / 100 / 12;
-  const totalNumberOfPayments = loanTenure * 12;
-  const monthlyPayment =
-    (principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalNumberOfPayments)) /
-    (Math.pow(1 + monthlyInterestRate, totalNumberOfPayments) - 1);
-  const totalPayment = monthlyPayment * totalNumberOfPayments;
-  const totalInterest = totalPayment - principal;
-
-  return {
-    monthlyPayment: monthlyPayment.toFixed(2),
-    totalInterestPayable: totalInterest.toFixed(2)
-  };
-}
-
-/**
- * Handles form submission and displays the loan details.
- */
 function handleFormSubmission() {
-  const principal = parseFloat(document.getElementById('principal').value);
-  const interestRate = parseFloat(document.getElementById('interest').value);
-  const loanTenure = parseFloat(document.getElementById('tenure').value);
-
-  const loanDetails = calculateLoanDetails(principal, interestRate, loanTenure);
-
-  document.getElementById('result').innerHTML = `
-    Monthly payment: $${loanDetails.monthlyPayment}<br>
-    Total interest payable: $${loanDetails.totalInterestPayable}
-  `;
+  const principalInput = document.getElementById("principal");
+  const interestInput = document.getElementById("interest");
+  const tenureInput = document.getElementById("tenure");
+  const result = document.getElementById("result");
+  
+  const principal = parseInt(principalInput.value.replace(/\$|,/g, ""));
+  const interest = parseFloat(interestInput.value);
+  const tenure = parseInt(tenureInput.value);
+  
+  const monthlyInterestRate = interest / 1200;
+  const totalPayments = tenure * 12;
+  const discountFactor = (Math.pow(1 + monthlyInterestRate, totalPayments) - 1) / (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalPayments));
+  const monthlyPayment = (principal / discountFactor).toFixed(2);
+  
+  result.innerText = `Monthly payment: $${formatNumberWithCommas(monthlyPayment)}`;
 }
+
+function formatNumberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const principalInput = document.getElementById("principal");
+principalInput.addEventListener("input", function() {
+  this.value = formatNumberWithCommas(this.value.replace(/\$|,/g, ""));
+});
