a786e67a8fe29a13f0199161eb7ef770b83226b4
Kenneth Wong <wongzhicongkenneth@outlook.com>
2023-04-11 15:33:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Function to calculate monthly payment
 function calculateMonthlyPayment(principal, interest, tenure) {
   const interestRate = interest / (12 * 100);
   const loanTenure = tenure * 12;
@@ -6,9 +7,31 @@ function calculateMonthlyPayment(principal, interest, tenure) {
   resultElement.textContent = `$${monthlyPayment.toFixed(2)}`;
 }
 
+// Function to add loan to table
+function addLoanToTable(principal, interest, tenure) {
+  const table = document.getElementById("loan-table");
+  const newRow = table.insertRow(-1);
+  const principalCell = newRow.insertCell(0);
+  const interestCell = newRow.insertCell(1);
+  const tenureCell = newRow.insertCell(2);
+  const paymentCell = newRow.insertCell(3);
+  
+  principalCell.innerHTML = `$${principal}`;
+  interestCell.innerHTML = `${interest}%`;
+  tenureCell.innerHTML = `${tenure} years`;
+  
+  const interestRate = interest / (12 * 100);
+  const loanTenure = tenure * 12;
+  const monthlyPayment = (principal * interestRate * (Math.pow(1 + interestRate, loanTenure))) / (Math.pow(1 + interestRate, loanTenure) - 1);
+  paymentCell.innerHTML = `$${monthlyPayment.toFixed(2)}`;
+}
+
+// Function to handle form submission
 function handleFormSubmission() {
   const principal = document.getElementById('principal').value;
   const interest = document.getElementById('interest').value;
   const tenure = document.getElementById('tenure').value;
+  
   calculateMonthlyPayment(principal, interest, tenure);
+  addLoanToTable(principal, interest, tenure);
 }
