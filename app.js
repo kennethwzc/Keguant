2dd4d97a94f19f4bcb033607414dc4e03b5e61cf
Kenneth Wong <wongzhicongkenneth@outlook.com>
2023-04-11 09:47:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
-const principalInput = document.getElementById("principal");
-const interestInput = document.getElementById("interest");
-const resultElement = document.getElementById("result");
-const tenure = document.getElementById("tenure");
+const LoanCalculator = (() => {
+  const principalInput = document.getElementById("principal");
+  const interestInput = document.getElementById("interest");
+  const resultElement = document.getElementById("result");
+  const tenure = document.getElementById("tenure");
 
-function formatPrincipalValue(value) {
-  const formattedValue = value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  return `$${formattedValue}`;
-}
+  function formatPrincipalValue(value) {
+    const formattedValue = value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return `$${formattedValue}`;
+  }
 
-function formatInterestValue(value) {
-  const formattedValue = value.replace(/(?!\d|%)/g, "");
-  return formattedValue ? `${formattedValue}%` : "";
-}
+  function formatInterestValue(value) {
+    const formattedValue = value.replace(/(?!\d|%)/g, "");
+    return formattedValue ? `${formattedValue}%` : "";
+  }
 
-function handlePrincipalInput() {
-  const value = principalInput.value.replace(/\D/g, "");
-  principalInput.value = formatPrincipalValue(value);
-}
+  function handleFormSubmission(event) {
+    event.preventDefault();
+    const principal = parseFloat(principalInput.value.replace(/[^0-9.-]+/g,""));
+    const interest = parseFloat(interestInput.value.replace(/[^0-9.-]+/g,"")) / 100;
+    const numberOfPayments = tenure.value * 12;
+    const monthlyInterestRate = interest / 12;
 
-function handleInterestInput() {
-  const value = interestInput.value.replace(/(?!\d|%)/g, "");
-  interestInput.value = formatInterestValue(value);
-}
+    const monthlyPayment =
+      (principal *
+        monthlyInterestRate *
+        Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
+      (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const principal = parseFloat(principalInput.value.replace(/[^0-9.-]+/g,""));
-  const interest = parseFloat(interestInput.value.replace(/[^0-9.-]+/g,"")) / 100;
-  const numberOfPayments = tenure.value * 12;
-  const monthlyInterestRate = interest / 12;
+    resultElement.textContent = `$${monthlyPayment.toFixed(2)} per month`;
+  }
 
-  const monthlyPayment =
-    (principal *
-      monthlyInterestRate *
-      Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
-    (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+  function init() {
+    const formElement = document.getElementById("loan-form");
+    formElement.addEventListener("submit", handleFormSubmission);
+  }
 
-  resultElement.textContent = `$${monthlyPayment.toFixed(2)} per month`;
-}
+  return { init };
+})();
+
+LoanCalculator.init();
