91067b8c19b6787d1b27213b307d4f3b2eb85718
Kenneth Wong <wongzhicongkenneth@outlook.com>
2023-04-11 14:31:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ function calculateMonthlyPayment(principal, interest, tenure) {
   const monthlyPayment = (principal * interestRate * (Math.pow(1 + interestRate, loanTenure))) / (Math.pow(1 + interestRate, loanTenure) - 1);
   const resultElement = document.getElementById("result");
   resultElement.textContent = `Monthly Payment: $${monthlyPayment.toFixed(2)}`;
-  return monthlyPayment;
 }
 
 function handleFormSubmission() {
   const principal = document.getElementById('principal').value;
   const interest = document.getElementById('interest').value;
   const tenure = document.getElementById('tenure').value;
-  const monthlyPayment = calculateMonthlyPayment(principal, interest, tenure);
-  displayResult(monthlyPayment);
+  calculateMonthlyPayment(principal, interest, tenure);
 }
